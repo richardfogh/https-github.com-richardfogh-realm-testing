205e1ec952f3823ce2798f00e9307c1ad10f921d
Richard Fogh <sackingen@outlook.com>
2022-07-25 16:55:47 -0700

    Initial Commit

---
+++ RealmTesting/RealmTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmTestingApp.swift
+//  RealmTesting
+//
+//  Created by Richard Fogh on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct RealmTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
